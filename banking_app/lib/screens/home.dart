1a42eb526b9744d0c8bbfab0f0ac34f66deefdf4
Alex <alexesc154@gmail.com>
2022-11-12 23:25:17 +0300

    create home.dart

--- banking_app/lib/screens/home.dart
+++ banking_app/lib/screens/home.dart
@@ -10,6 +10,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(children: [
+        appbar(),
+        CardList(),
+        RecentTransactions(),
+      ]),
+    );
   }
 }
