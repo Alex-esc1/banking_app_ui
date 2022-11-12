0582b5340b40ae8612c8865a89b9926f3e2e6cbc
Alex <alexesc154@gmail.com>
2022-11-12 23:49:12 +0300

    add component cards.dart

--- banking_app/lib/components/cards.dart
+++ banking_app/lib/components/cards.dart
@@ -10,6 +10,22 @@ class CardList extends StatefulWidget {
 class _CardListState extends State<CardList> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 32.0, left: 15.0, right: 15.0, bottom: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Your Cards'),
+                Text('See All'),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
