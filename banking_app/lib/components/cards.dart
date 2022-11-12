6e2c06a6bf07baaa14d3a07a4f9b74afd043014a
Alex <alexesc154@gmail.com>
2022-11-12 23:30:50 +0300

    add components

---
+++ banking_app/lib/components/cards.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardList extends StatefulWidget {
+  const CardList({super.key});
+
+  @override
+  State<CardList> createState() => _CardListState();
+}
+
+class _CardListState extends State<CardList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- banking_app/lib/screens/home.dart
+++ banking_app/lib/screens/home.dart
@@ -1,3 +1,6 @@
+import 'package:banking_app/components/appbar.dart';
+import 'package:banking_app/components/cards.dart';
+import 'package:banking_app/components/recentTransactions.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,7 +15,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Container(
       child: Column(children: [
-        appbar(),
+        Appbar(),
         CardList(),
         RecentTransactions(),
       ]),
