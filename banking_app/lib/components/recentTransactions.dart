7336b434a380230a00c7976abfb7934516f19911
Alex <alexesc154@gmail.com>
2022-11-13 21:22:21 +0300

    Create transactionCard.dart

--- banking_app/lib/components/recentTransactions.dart
+++ banking_app/lib/components/recentTransactions.dart
@@ -1,3 +1,5 @@
+import 'package:banking_app/utilities/themeStyles.dart';
+import 'package:banking_app/widget/transactionCard.dart';
 import 'package:flutter/material.dart';
 
 class RecentTransactions extends StatefulWidget {
@@ -10,6 +12,43 @@ class RecentTransactions extends StatefulWidget {
 class _RecentTransactionsState extends State<RecentTransactions> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Expanded(
+        child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+              left: 15.0, right: 15.0, bottom: 16.0, top: 32.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Recent Transactions',
+                style: ThemeStyles.primaryTitle,
+              ),
+              Text(
+                'See All',
+                style: ThemeStyles.seeAll,
+              )
+            ],
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: ListView(
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            children: [
+              TransactionCard(
+                color: Colors.black,
+                letter: 'F',
+                title: 'Fintory Gmbh',
+                subTitle: 'Finance Landing Page',
+                price: '- 5.720,30 €',
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
